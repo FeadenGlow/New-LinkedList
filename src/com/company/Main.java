944ff66eb93a02947557b980d077eb5eab923a59
Minezor <minezor.github.com>
2020-05-23 11:58:13 +0300

    Added the Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,17 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) throws SizeException{
+        LinkedList2 list = new LinkedList2();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+        list.set(0, "4");
+        System.out.println(list.get(0));
+        list.remove(0);
+        System.out.println(list.get(0));
+        list.clear();
 
     }
 }
